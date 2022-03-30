15fe029ef3e37e1b63cf2edc7f4e555f836cf472
loolloo <47209856+loolloo@users.noreply.github.com>
2022-03-30 14:23:17 +0200

    added crypto writeup (and cleaned some files)

--- Crypto/tallest_header/encrypt.py
+++ Crypto/tallest_header/encrypt.py
@@ -9,6 +9,7 @@ def encrypt(key, plaintext):
         ciphertext += " "
     return ciphertext[:-1]
 
+print(encrypt([2,1,3,5,4], "VISHWACTF HELLO WORLD"))
 print(encrypt([2,1,3,5,4], "RT1KC _YH43 3DRW_ T1HP_ R3M7U TA1N0"))
 
 """
